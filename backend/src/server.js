0f5e698243b1e38644b9ce719b311e0c4735d6f7
TejasGtech <ghuletejas26@gmail.com>
2026-02-13 13:14:38 +0530

    Adding port correctly

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,9 @@ const connectDB = require('./db/db')
 
 connectDB();
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000 || process.env.PORT,()=>{
+app.listen(PORT,()=>{
     console.log('Server start....');
 })
 
